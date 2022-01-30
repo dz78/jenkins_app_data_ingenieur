f685a69e95755db6d653fabdcbd279e75c40d11e
dz78 <37299705+dz78@users.noreply.github.com>
2022-01-30 20:05:01 +0100

    second commit

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http');
+/*var http = require('http');
 
 function onRequest(request, response ) {
     response.writeHead(200, {'Content-type': 'text/plain'});
@@ -6,4 +6,19 @@ function onRequest(request, response ) {
     response.end();
 }
 
-http.createServer(onRequest).listen(8000);
+http.createServer(onRequest).listen(8080); */
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  //res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
